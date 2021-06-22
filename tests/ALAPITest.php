234905a004c5f68d671a4da4a8d668e0566fcde6
Alone88 <im@alone88.cn>
2021-06-22 21:43:18 +0800

    :construction_worker: 添加 phpunit

---
+++ tests/ALAPITest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the ALAPI-SDK/php-sdk.
+ * (c) Alone88 <im@alone88.cn>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace ALAPI\Tests;
+
+use ALAPI\Client;
+use ALAPI\Exception\ALAPIException;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+class ALAPITest extends TestCase
+{
+    public function testAlapi()
+    {
+        $client = new Client('token');
+
+        try {
+            $result = $client->setApi('/api/hitokoto')
+                ->setParam('type', 'a')
+                ->request();
+            Assert::assertEquals(100, $result->getCode());
+        } catch (ALAPIException $e) {
+        }
+    }
+
+    public function testUpload()
+    {
+        $client = new Client('token');
+        try {
+            $result = $client->setApi('/api/image')
+                ->setParam('image', 'tests/img.png', true)
+                ->setParam('type', 'alapi')
+                ->request();
+            Assert::assertEquals(100, $result->getCode());
+        } catch (ALAPIException $e) {
+        }
+    }
+}
